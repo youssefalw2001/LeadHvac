aaf0cec9fe7cd087ca6bee5c565ed3a5489b4a1b
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-31 21:17:06 +0300

    Enable admin inbox overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { AdminBoot } from './AdminBoot';
 import { CaptureBoot } from './CaptureBoot';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CaptureBoot />
+    <AdminBoot />
     <App />
   </React.StrictMode>
 );
