4ca8907ec4b875cd8a72e3c9e60c930eff283a5e
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-31 21:00:31 +0300

    Enable scan form lead capture

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { CaptureBoot } from './CaptureBoot';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CaptureBoot />
     <App />
   </React.StrictMode>
 );
