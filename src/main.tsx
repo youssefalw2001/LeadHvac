d0aedcc6012420d32928ad71f53456ea77d8b567
youssefalw2001 <youssefalw2001@gmail.com>
2026-06-02 18:39:14 +0300

    Enable safe radar source panel

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { AdminBoot } from './AdminBoot';
 import { AdminLauncher } from './AdminLauncher';
 import { CaptureBoot } from './CaptureBoot';
 import { PublicAppRouter } from './PublicAppRouter';
+import { RadarSourcePanelBoot } from './RadarSourcePanelBoot';
 import './tailwind.css';
 import './index.css';
 import './premium.css';
 import './executive.css';
 import './showcase.css';
+import './safe-source-panel.css';
 import './mobile-fix.css';
 import './admin.css';
 
@@ -16,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CaptureBoot />
     <AdminBoot />
+    <RadarSourcePanelBoot />
     <PublicAppRouter />
     <AdminLauncher />
   </React.StrictMode>
