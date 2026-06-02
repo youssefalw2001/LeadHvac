b930689f7c1b9c92f4f347cb0bdfd86fb68ebc7a
youssefalw2001 <youssefalw2001@gmail.com>
2026-06-02 19:22:35 +0300

    Enable four-trade mode and radar polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { AdminBoot } from './AdminBoot';
 import { AdminLauncher } from './AdminLauncher';
 import { CaptureBoot } from './CaptureBoot';
+import { FourTradeModeBoot } from './FourTradeModeBoot';
 import { PublicAppRouter } from './PublicAppRouter';
 import { RadarSourcePanelBoot } from './RadarSourcePanelBoot';
 import './tailwind.css';
@@ -11,6 +12,7 @@ import './premium.css';
 import './executive.css';
 import './showcase.css';
 import './safe-source-panel.css';
+import './radar-polish.css';
 import './mobile-fix.css';
 import './admin.css';
 
@@ -18,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CaptureBoot />
     <AdminBoot />
+    <FourTradeModeBoot />
     <RadarSourcePanelBoot />
     <PublicAppRouter />
     <AdminLauncher />
